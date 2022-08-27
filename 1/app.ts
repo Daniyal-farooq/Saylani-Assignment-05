7ef1b8ba06ac813944ebe2d56dda39a076b4a100
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-27 16:18:40 +0500

    assignment-05

---
+++ 1/app.ts
@@ -0,0 +1,16 @@
+// prog to find maxbetwwen 2
+ var n1:number = 10 
+ var n2:number = -10
+
+if ( n1 > n2 )
+{
+    console.log (n1," is greater"  )
+}
+if ( n2 > n1 )
+{
+    console.log (n2," is greater"  )
+}
+else
+{
+    console.log ("Both are  equal")
+}
