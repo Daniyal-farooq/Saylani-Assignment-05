1d295fc7d86e1f6e63d1a430ca98d91482a207e0
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-28 23:00:03 +0500

    Problem 11

---
+++ 11/app.ts
@@ -0,0 +1,36 @@
+var input:number = prompt("Enter number to get its corresponding day of the week. Ex# 1 = Monday ")
+var input1:number = Number(input)
+if (input1 == 1)
+{
+    console.log("Monday");
+    
+}
+if (input1 == 2)
+{
+    console.log("Tuesday");
+    
+}
+if (input1 == 3)
+{
+    console.log("Wednesday");
+}
+if (input1 == 4)
+{
+    console.log("Thursday");
+    
+}
+if (input1 == 5)
+{
+    console.log("Friday");
+    
+}
+if (input1 == 6)
+{
+    console.log("Saturday");
+    
+}
+if (input1 == 7)
+{
+    console.log("Sunday");
+    
+}
