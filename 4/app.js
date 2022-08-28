8b8e2e98bf3d7a9c55e3ab91f2a533211799fbe9
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-28 10:50:44 +0500

    4th problem updated

---
+++ 4/app.js
@@ -0,0 +1,5 @@
+var test = 110;
+if (test % 5 == 0 && test % 11 == 0) {
+    var result = "value ".concat(test, " is divisible by 11 and 5");
+    console.log(result);
+}
---
+++ 4/app.ts
@@ -0,0 +1,6 @@
+var test:number  = 110
+if (test%5==0 && test%11==0) {
+    var result:string = `value ${test} is divisible by 11 and 5`;
+    console.log(result);
+    
+}
