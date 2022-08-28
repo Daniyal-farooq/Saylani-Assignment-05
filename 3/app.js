38a90ecd04dd0b65e736e6940f09f5f61585b206
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-28 10:26:30 +0500

    third program

---
+++ 3/app.js
@@ -0,0 +1,10 @@
+var n1 = 10;
+if (n1 < 0) {
+    console.log(n1, " is smaller than 0 ");
+}
+else if (n1 > 0) {
+    console.log(n1, " is greater than 0 ");
+}
+else {
+    console.log("Number is 0");
+}
---
+++ 3/app.ts
@@ -0,0 +1,11 @@
+var n1:number = 10
+if (n1<0) {
+    console.log(n1,' is smaller than 0 ');
+}
+else if (n1>0) {
+    console.log(n1, is greater than 0);
+}
+else
+{
+    console.log('Number is 0')
+}
