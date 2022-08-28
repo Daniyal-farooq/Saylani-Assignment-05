1919683240f34516a431e6e317070f04b124da99
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-28 16:01:48 +0500

    Problem 20

---
+++ 20/app.ts
@@ -0,0 +1,13 @@
+var inputs:Number = prompt("Enter your base salary : ")
+var s:Number
+s = Number(inputs)
+if(s<=10000){
+    s+= (s/100 * 20) + (s/100 * 80)
+}
+else if(s <= 20000){
+    s+= (s/100 * 25) + (s/100 * 90)
+}
+else if(s > 20000){
+    s+= (s/100 * 30) + (s/100 * 95)
+}
+console.log(`Gross salary is : ${s}`);
