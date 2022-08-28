527d867ab31761993edce4748279693154768504
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-28 13:44:44 +0500

    Problem 8 updated

---
+++ 8/app.js
@@ -0,0 +1,8 @@
+var test = "B";
+if (test == "A" || test == "a" || test == "E" || test == "e"
+    || test == "I" || test == "i" || test == "O" || test == "o" || test == "U" || test == "u") {
+    console.log("".concat(test, " is a Vowel"));
+}
+else {
+    console.log("".concat(test, " is consonent"));
+}
---
+++ 8/app.ts
@@ -0,0 +1,8 @@
+var test:string = "B"
+if (test == "A" || test == "a"||test == "E" || test == "e"
+||test == "I" || test == "i"||test == "O" || test == "o"||test == "U" || test == "u"){
+    console.log(`${test} is a Vowel`);
+}
+else{
+    console.log(`${test} is consonent`);
+}
