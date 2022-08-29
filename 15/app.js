2fd20ffd0641ed372897cc3addec003bafa3651e
Daniyal-farooq <mdanifarooq@gmail.com>
2022-08-29 17:35:08 +0500

    Problem 15

---
+++ 15/app.js
@@ -0,0 +1,12 @@
+var s1 = prompt("Enter the First side");
+var s2 = prompt("Enter the Second side");
+var s3 = prompt("Enter the Third side");
+var side1 = Number(s1);
+var side2 = Number(s3);
+var side3 = Number(s3);
+if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2) {
+    console.log("The sides of the triangle prove that it is valid traiangle");
+}
+else {
+    console.log("The sides of the triangle prove that it is not valid traiangle");
+}
